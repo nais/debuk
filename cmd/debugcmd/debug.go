b9997e57ec75e12b6e040b0f0697d7e80865ba22
Youssef Bel Mekki <38552193+ybelMekk@users.noreply.github.com>
2024-11-05 14:20:08 +0100

    chore(mod): add commands
    Co-authored-by: Sindre Rødseth Hansen <sindre.rodseth.hansen@nav.no>
    Co-authored-by: Carl Hedgren <carl.hedgren@nav.no>

--- cmd/debugcmd/debug.go
+++ cmd/debugcmd/debug.go
@@ -4,18 +4,26 @@ import (
 	"fmt"
 	"github.com/nais/cli/pkg/debug"
 	"github.com/nais/cli/pkg/k8s"
-
 	"github.com/nais/cli/pkg/metrics"
 	"github.com/urfave/cli/v2"
 )
 
 // kubectl debug --profile=restricted -it johnnyjob-7hgx4 --image=busybox:1.28
+const (
+	namespaceFlagName = "namespace"
+	contextFlagName   = "context"
+	debugImageDefault = "europe-north1-docker.pkg.dev/nais-io/nais/images/debug:latest"
+)
 
 func Command() *cli.Command {
 	return &cli.Command{
 		Name:      "debug",
 		Usage:     "Debug nais application",
 		ArgsUsage: "podname",
+		Flags: []cli.Flag{
+			namespaceFlag(),
+			kubeConfigFlag(),
+		},
 		Before: func(context *cli.Context) error {
 			metrics.AddOne("debug_total")
 			if context.Args().Len() < 1 {
@@ -25,10 +33,59 @@ func Command() *cli.Command {
 
 			return nil
 		},
-		Action: func(context *cli.Context) error {
-			d := debug.Setup(k8s.SetupControllerRuntimeClient())
+		Action: func(cCtx *cli.Context) error {
+			cfg := makeConfig(cCtx)
+			cluster := cCtx.String(contextFlagName)
+			namespace := cCtx.String(namespaceFlagName)
+			client := k8s.SetupControllerRuntimeClient(k8s.WithKubeContext(cluster))
+			cfg.Namespace = client.CurrentNamespace
+			if namespace != "" {
+				cfg.Namespace = namespace
+			}
 
+			clientset, err := k8s.SetupClientGo(cluster)
+			if err != nil {
+				return err
+			}
+			dg := debug.Setup(clientset, cfg)
+			p, err := dg.Debug()
+			if err != nil {
+				return fmt.Errorf("error debugging instance: %w", err)
+			}
+			err = dg.AttachToEphemeralContainer(p)
+			if err != nil {
+				return fmt.Errorf("error attaching to ephemeral container: %w", err)
+			}
 			return nil
 		},
+	}
+}
+
+func namespaceFlag() *cli.StringFlag {
+	return &cli.StringFlag{
+		Name:        namespaceFlagName,
+		DefaultText: "The namespace from your current kubeconfig context",
+		Usage:       "The kubernetes `NAMESPACE` to use",
+		Aliases:     []string{"n"},
+	}
+}
+
+func kubeConfigFlag() *cli.StringFlag {
+	return &cli.StringFlag{
+		Name:        contextFlagName,
+		Aliases:     []string{"c"},
+		Usage:       "The kubeconfig `CONTEXT` to use",
+		DefaultText: "The current context in your kubeconfig",
+	}
+}
+
+func makeConfig(cCtx *cli.Context) debug.Config {
+	podName := cCtx.Args().Get(0)
+	namespace := cCtx.Args().Get(1)
+
+	return debug.Config{
+		PodName:    podName,
+		Namespace:  namespace,
+		DebugImage: debugImageDefault,
 	}
 }
--- pkg/debug/debug.go
+++ pkg/debug/debug.go
@@ -2,20 +2,120 @@ package debug
 
 import (
 	"context"
-	ctrl "sigs.k8s.io/controller-runtime/pkg/client"
+	"fmt"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/rand"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/remotecommand"
+	"os"
 )
 
 type Debug struct {
-	Ctx    context.Context
-	Client ctrl.Client
+	ctx       context.Context
+	clientset kubernetes.Interface
+	cfg       Config
 }
 
-func Setup(client ctrl.Client) *Debug {
+type Config struct {
+	Namespace  string
+	Context    string
+	PodName    string
+	DebugImage string
+}
+
+func Setup(clientset kubernetes.Interface, cfg Config) *Debug {
 	return &Debug{
-		Ctx:    context.Background(),
-		Client: client,
+		ctx:       context.Background(),
+		clientset: clientset,
+		cfg:       cfg,
 	}
 }
 
-func (d *Debug) debug() {
+func (d *Debug) Debug() (*v1.Pod, error) {
+	pod, err := d.clientset.CoreV1().Pods(d.cfg.Namespace).Get(d.ctx, d.cfg.PodName, metav1.GetOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to get pod: %w", err)
+	}
+
+	// Define the ephemeral container with security context
+	debugContainer := &v1.EphemeralContainer{
+		EphemeralContainerCommon: v1.EphemeralContainerCommon{
+			Name:            fmt.Sprintf("debug-%s", rand.IntnRange(1000, 9999)),
+			Image:           d.cfg.DebugImage,
+			Command:         []string{"sh"},
+			Stdin:           true,
+			TTY:             true,
+			SecurityContext: securityContext(),
+		},
+	}
+
+	pod.Spec.EphemeralContainers = append(pod.Spec.EphemeralContainers, *debugContainer)
+
+	p, err := d.clientset.CoreV1().Pods(d.cfg.Namespace).UpdateEphemeralContainers(d.ctx, d.cfg.PodName, pod, metav1.UpdateOptions{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to update pod: %w", err)
+	}
+
+	fmt.Println("Debug container added successfully, attaching to it...")
+	return p, err
+}
+
+func securityContext() *v1.SecurityContext {
+	secCon := &v1.SecurityContext{
+		Capabilities: &v1.Capabilities{
+			Drop: []v1.Capability{"ALL"},
+		},
+	}
+	*secCon.RunAsUser = 1000
+	*secCon.RunAsGroup = 1000
+	*secCon.Privileged = false
+	*secCon.RunAsNonRoot = true
+	*secCon.ReadOnlyRootFilesystem = true
+	*secCon.AllowPrivilegeEscalation = false
+	return secCon
+}
+
+func (d *Debug) AttachToEphemeralContainer(config *rest.Config, containerName string) error {
+	req := d.clientset.CoreV1().RESTClient().
+		Post().
+		Resource("pods").
+		Name(d.cfg.PodName).
+		Namespace(d.cfg.Namespace).
+		SubResource("attach").
+		Param("container", containerName).
+		Param("stdin", "true").
+		Param("stdout", "true").
+		Param("stderr", "true").
+		Param("tty", "true")
+
+	exec, err := remotecommand.NewSPDYExecutor(config, "POST", req.URL())
+	if err != nil {
+		return fmt.Errorf("failed to create executor: %w", err)
+	}
+
+	// Set up input/output streams
+	err = exec.Stream(remotecommand.StreamOptions{
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+		Tty:    true,
+	})
+	if err != nil {
+		return fmt.Errorf("error in stream: %w", err)
+	}
+
+	return nil
+}
+
+func (d *Debug) tidy() {
+	// Delete the pod
+	err := d.clientset.CoreV1().Pods(d.cfg.Namespace).Delete(d.ctx, d.cfg.PodName, metav1.DeleteOptions{})
+	if err != nil {
+		fmt.Printf("Failed to delete pod: %v\n", err)
+		return
+	}
+
+	fmt.Println("Pod deleted successfully. Kubernetes will recreate it if managed by a controller.")
 }
